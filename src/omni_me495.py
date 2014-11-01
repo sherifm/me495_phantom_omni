634185db79b8a5aeb187ab0305b2062e040e7832
Sherif <sherif@puppeteer-laptop.(none)>
2014-11-01 14:08:35 -0500

    discovered hidden indentation error that resulted in empty topic

--- src/omni_me495.py
+++ src/omni_me495.py
@@ -4,27 +4,26 @@ roslib.load_manifest('me495_phantom_omni')
 import rospy
 import math
 import tf
-import geometry_msgs.msg
+from geometry_msgs.msg import Twist,Vector3
+from tf.transformations import euler_from_quaternion
 
 if __name__=='__main__':
 	rospy.init_node('mini_omni')
 
 	listener = tf.TransformListener()
 
-	Tsb = rospy.Publisher('stylus_to_base_transf',geometry_msgs.msg.Twist)
+	Tsb = rospy.Publisher('stylus_to_base_transf',Twist)
 
 	rate = rospy.Rate(10.0)
 	while not rospy.is_shutdown():
 		try:
-			(trans,rot) = listener.lookupTransform('base','stylus',rospy.Time(0))
+			(trans,quat) = listener.lookupTransform('base','stylus',rospy.Time(0))
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			continue
-			
-        S = geometry_msgs.msg.Twist()
-     	S=(trans,rot) 
-        Tsb.publish(S)
-
-    	rate.sleep()
+		rot= euler_from_quaternion(quat)
+		S=Twist(Vector3(trans[0],trans[1],trans[2]),(Vector3(rot[0],rot[1],rot[2])))
+		Tsb.publish(S)
+		rate.sleep()
 
 
 
