d9b38988f8d3733d7b196a725db1c25bdb392d70
elmuhn <ngulmus@u.northwestern.edu>
2014-11-03 10:29:12 -0600

    commit

--- src/learning_kdl.py
+++ src/learning_kdl.py
@@ -3,8 +3,7 @@ from urdf_parser_py.urdf import URDF
 from pykdl_utils.kdl_parser import kdl_tree_from_urdf_mode
 from omni_description import urdf
 
-
-# robot = URDF.load_from_parameter_server(verbose=False)
+robot = URDF.load_from_parameter_server(verbose=False)
 tree = kdl_tree_from_urdf_model(robot)
 print tree.getNrOfSegments()
 chain = tree.getChain(base_link, end_link)
