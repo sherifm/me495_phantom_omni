e1529d0088a1f9a73d8c7743da5c7a8d3dd6507a
Sherif <sherif@puppeteer-laptop.(none)>
2014-11-04 15:02:15 -0600

    working on fource_controller

--- src/omni_mini_proj.py
+++ src/omni_mini_proj.py
@@ -29,47 +29,68 @@ class OmniMiniProj:
 
 	def __init__(self):
 		
-		#tf listeners and publishers
-		self.tfListener = tf.TransformListener() #tf.TransfromListener is a subclass that subscribes to the "/tf" message topic and adds 
-		#transform data to the tf data structure
+		#Create a tf listener
+		self.tfListener = tf.TransformListener() 
+		#tf.TransfromListener is a subclass that subscribes to the "/tf" message topic and adds transform 
+		#data to the tf data structure. As a result the object is up to date with all current transforms
 
-		self.Tsb = rospy.Publisher('stylus_to_base_transf',Twist) #Create a publisher to a new topic name calle "stylus_to_base_transf with 
-		#a message type Twist"
+		#Create a publisher to a new topic name called "stylus_to_base_transf with a message type Twist"
+		self.Tsb = rospy.Publisher('stylus_to_base_transf',Twist) 
 
-		#Find the omni from parameter server
-		self.omni = URDF.from_parameter_server() #A node that initializes and runs the Phantom Omni has to be running in the background for 
-		#from_parameter_server to to find the parameter
+		#Find the omni from the parameter server
+		self.omni = URDF.from_parameter_server() 
+		#Note: 	A node that initializes and runs the Phantom Omni has to be running in the background for 
+		#		from_parameter_server to to find the parameter.
+		#		Older versions of URDF label the function "load_from_parameter_server"
 
-		#kdl subscribers and publishers
-		self.join_state_sub = rospy.Subscriber("omni1_joint_states", JointState, self.get_joint_states)
+
+		#Subscribe to the "omni1_joint_states" topic, which provides the joint states measured by the omni in a
+		# ROS message of the type sensor_msgs/JointState. Every time a message is published to that topic run the 
+		#callback function self.get_joint_states, which is defined below.
+		self.joint_state_sub = rospy.Subscriber("omni1_joint_states", JointState, self.get_joint_states)
 
 		#OmniMiniProjTimers
-		self.timer1 = rospy.Timer(rospy.Duration(0.2), self.timer_callback)
-
-		
+		self.timer1 = rospy.Timer(rospy.Duration(0.2), self.timer_callback) 
+		#Run a timer to manipulate the class structure as needed. The timer's 
+		#callback function "timer_callback" then calls the desired functions
 
 		return
 
+	#The timer_callback runs every time timer1 triggers. In this example, it merely calls other functions. Generally,
+	#it may include further calculations, just like any regular function.
 	def timer_callback(self, data):
+
 		self.base_to_stylus()
 		self.kdl_forward_kinematics()
 
 		return
 
+	#get_joint_states extracts the joint agles from the JointState message  (angles, agular velocity, ...), which is
+	#provided by the 'joint_state_sub' subscriber.
 	def get_joint_states(self,data):
+
 		self.q_sensors = data.position
 
 		return
 
-
+	#base_to_stylus uses tf to look up the transfrom from the frame located at the root of the Omni's URDF (/base) to the frame
+	#at the end of the URDF (/stylus).
 	def base_to_stylus (self):
 
 		try:
 			(self.transl,self.quat) = self.tfListener.lookupTransform('base','stylus',rospy.Time(0))
+			#lookupTransrom is a method which returns the transfrom between two coordinate frames. 
+			#lookupTransfrom returns a translation and a quaternion
 		except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
 			pass
-		self.rot= euler_from_quaternion(self.quat)
+			#If exceptions occur, pass skip trying to lookup the transform. 
+			#It is encouraged to publish a logging message to rosout with rospy. 
+			#i.e: 
+			#rospy.logerr("Could not transform from %s to %s,"base","stylus")
+			
+		self.rot= euler_from_quaternion(self.quat) #Get euler angles from the quaternion
 		self.transf = Twist(Vector3(self.transl[0],self.transl[1],self.transl[2]),(Vector3(self.rot[0],self.rot[1],self.rot[2])))
+		#Store the transformation in a format compatible with gemoetr_msgs/Twist
 		self.Tsb.publish(self.transf)
 		
 		# self.test1 = "Publishing base to stylus"
@@ -106,7 +127,7 @@ def main():
     rospy.init_node('omni_mini_proj')
 
     try:
-        sim = OmniMiniProj()
+        demo = OmniMiniProj()
     except rospy.ROSInterruptException: pass
 
     rospy.spin()
